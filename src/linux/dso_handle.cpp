08db8eeeb68e7ba43ce7c8451eb127c841ae726e
Jan Koester <jan.koester@gmx.net>
2021-09-27 21:49:48 +0200

    unwind code seperated

--- src/linux/dso_handle.cpp
+++ src/linux/dso_handle.cpp
@@ -1,5 +1,4 @@
 #include <unwind.h>
-#include <inttypes.h>
 
 #include "sysexception.h"
 
@@ -14,29 +13,3 @@ static int errno;
 __attribute__((visibility("hidden"))) int* __errno_location(void) { 
     return &errno; 
 }
-
-extern "C" {
-    _Unwind_Reason_Code __gxx_personality_v0 (
-                     int version, _Unwind_Action actions, uint64_t exceptionClass,
-                     _Unwind_Exception* unwind_exception, _Unwind_Context* context){
-        if (unwind_exception == 0 || context == 0)
-            return _URC_FATAL_PHASE1_ERROR;
-        
-        
-//         _URC_NO_REASON;
-//   _URC_FOREIGN_EXCEPTION_CAUGHT;
-// 
-//   _URC_FATAL_PHASE2_ERROR ;
-//   _URC_NORMAL_STOP;
-// 
-//   _URC_END_OF_STACK;
-//   _URC_HANDLER_FOUND;
-//   _URC_INSTALL_CONTEXT;
-//   _URC_CONTINUE_UNWIND;
-        return _URC_FATAL_PHASE1_ERROR;
-    }
-    
-    void _Unwind_Resume(struct _Unwind_Exception * object){
-        
-    }
-};
