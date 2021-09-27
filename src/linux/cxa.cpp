509bc353af59e874c45c7f5808f96374ebd309ae
Jan Koester <jan.koester@gmx.net>
2021-09-27 21:42:55 +0200

    cxa function moved to cxa.cpp

---
+++ src/linux/cxa.cpp
@@ -0,0 +1,108 @@
+#include <cxxabi.h>
+#include <typeinfo>
+
+namespace __cxxabiv1 {
+    bool __si_class_type_info::__do_dyncast(
+        ptrdiff_t __src2dst, __sub_kind __access_path,
+        const __class_type_info* __dst_type, const void* __obj_ptr,
+        const __class_type_info* __src_type, const void* __src_ptr,
+        __dyncast_result& __result) const{
+            
+        }
+        
+        
+        
+        bool __si_class_type_info::__do_upcast(
+            const __class_type_info*__dst, const void*__obj,
+            __upcast_result& __restrict __result) const{
+                
+        };
+            
+        __si_class_type_info::~__si_class_type_info(){
+                
+        }
+            
+        __class_type_info::~__class_type_info(){
+                
+        }
+            
+        __si_class_type_info::__sub_kind 
+        __class_type_info::__do_find_public_src(long, 
+            void const*, 
+            __cxxabiv1::__class_type_info const*, 
+            void const*) const{
+                
+        };
+                                                                
+        __si_class_type_info::__sub_kind
+        __si_class_type_info::__do_find_public_src(
+            long, 
+            void const*,
+            __class_type_info const*,
+            void const*) const{
+                                                                        
+        };
+                                                                    
+        bool __class_type_info::__do_catch(type_info const*, 
+            void**, 
+            unsigned int) const{
+                                                                                                                       
+        };
+                                                                                                                   
+        bool __class_type_info::__do_upcast(
+            __class_type_info const*, 
+            void**) const{
+                                                                                                                                                                       
+        };
+                                                                                                                                                                   
+        bool __class_type_info::__do_upcast(
+            __class_type_info const*,
+            void const*, 
+            __class_type_info::__upcast_result&) const{
+                
+        }
+        
+        bool __class_type_info::__do_dyncast(long,                                                                                                                                                                                                                                                                                                                          __class_type_info::__sub_kind,                                                                                                                                                               __class_type_info const*, 
+        void const*,                                                                                                                                                                                                                                        __class_type_info const*, 
+        void const*, 
+        __class_type_info::__dyncast_result&) const{
+                                                
+        };
+};
+
+extern "C" {
+    int __cxa_atexit(void (*f)(void *), void *objptr, void *dso){
+        return 0;
+    };
+    
+    int __cxa_finalize(void *f){
+
+    };
+    
+    void __cxa_call_unexpected(void *u){
+        
+    };
+    
+    void *__cxa_begin_catch(void *c) _GLIBCXX_NOTHROW{
+    
+    };
+    
+    int __cxa_guard_acquire(__cxxabiv1::__guard *guard_object){
+        return 0;
+    };
+    
+    void __cxa_guard_release(__cxxabiv1::__guard *guard_object) _GLIBCXX_NOTHROW{
+        
+    };
+    
+    void *__cxa_allocate_exception(unsigned long thrown_size){
+    };
+    
+    void __cxa_throw (void *thrown_exception,std::type_info *tinfo, void (*dest) (void *) ){
+    };
+    
+    void __cxa_free_exception(void *thrown_exception){
+        
+    };
+
+};
--- src/linux/dso_handle.cpp
+++ src/linux/dso_handle.cpp
@@ -1,7 +1,5 @@
 #include <unwind.h>
 #include <inttypes.h>
-#include <typeinfo>
-#include <cxxabi.h>
 
 #include "sysexception.h"
 
@@ -16,73 +14,6 @@ static int errno;
 __attribute__((visibility("hidden"))) int* __errno_location(void) { 
     return &errno; 
 }
-
-bool __cxxabiv1::__si_class_type_info::__do_dyncast(
-    ptrdiff_t __src2dst, __sub_kind __access_path,
-    const __class_type_info* __dst_type, const void* __obj_ptr,
-    const __class_type_info* __src_type, const void* __src_ptr,
-    __dyncast_result& __result) const{
-        
-}
-    
-
-                                                           
-bool __cxxabiv1::__si_class_type_info::__do_upcast(
-    const __class_type_info*__dst, const void*__obj,
-    __upcast_result& __restrict __result) const{
-        
-};
-                                                                                                         
-__cxxabiv1::__si_class_type_info::~__si_class_type_info(){
-        
-}
-
-__cxxabiv1::__class_type_info::~__class_type_info(){
-        
-}
-
-__cxxabiv1::__si_class_type_info::__sub_kind 
-__cxxabiv1::__class_type_info::__do_find_public_src(long, 
-    void const*, 
-    __cxxabiv1::__class_type_info const*, 
-    void const*) const{
-};
-
-__cxxabiv1::__si_class_type_info::__sub_kind
-__cxxabiv1::__si_class_type_info::__do_find_public_src(
-    long, 
-    void const*,
-    __cxxabiv1::__class_type_info const*,
-    void const*) const{
-    
-};
-
-bool __cxxabiv1::__class_type_info::__do_catch(type_info const*, 
-                                               void**, 
-                                               unsigned int) const{
-    
-};
-
-bool __cxxabiv1::__class_type_info::__do_upcast(__cxxabiv1::__class_type_info const*, 
-                                                void**) const{
-    
-};
-
-bool __cxxabiv1::__class_type_info::__do_upcast(__cxxabiv1::__class_type_info const*, 
-                                                void const*, 
-                                                __cxxabiv1::__class_type_info::__upcast_result&) const{
-    
-}
-
-bool __cxxabiv1::__class_type_info::__do_dyncast(long, 
-                                                 __cxxabiv1::__class_type_info::__sub_kind, 
-                                                 __cxxabiv1::__class_type_info const*, void const*, 
-                                                 __cxxabiv1::__class_type_info              const*, 
-                                                 void const*, 
-                                                 __cxxabiv1::__class_type_info::__dyncast_result&) const{
-    
-};
-
 
 extern "C" {
     _Unwind_Reason_Code __gxx_personality_v0 (
@@ -108,38 +39,4 @@ extern "C" {
     void _Unwind_Resume(struct _Unwind_Exception * object){
         
     }
-    
-    int __cxa_atexit(void (*f)(void *), void *objptr, void *dso){
-        return 0;
-    };
-    
-    int __cxa_finalize(void *f){
-
-    };
-    
-    void __cxa_call_unexpected(void *u){
-        
-    };
-    
-    void *__cxa_begin_catch(void *c) _GLIBCXX_NOTHROW{
-    
-    };
-    
-    int __cxa_guard_acquire(__cxxabiv1::__guard *guard_object){
-        return 0;
-    };
-    
-    void __cxa_guard_release(__cxxabiv1::__guard *guard_object) _GLIBCXX_NOTHROW{
-        
-    };
-    
-    void *__cxa_allocate_exception(unsigned long thrown_size){
-    };
-    
-    void __cxa_throw (void *thrown_exception,std::type_info *tinfo, void (*dest) (void *) ){
-    };
-    
-    void __cxa_free_exception(void *thrown_exception){
-        
-    };
 };
