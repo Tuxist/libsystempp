130ddd70381962a8bbfb04fddf282386e87cec0a
Jan Koester <jan.koester@gmx.net>
2021-12-22 16:21:38 +0100

    fixed header

--- src/libcmd/cmd.h
+++ src/libcmd/cmd.h
@@ -25,6 +25,8 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
 
+#include <cstddef>
+
 #pragma once
 
 namespace libsystempp {
