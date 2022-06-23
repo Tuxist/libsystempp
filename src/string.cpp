14762745e569c1903bcfeaf9ca6ce6f8e6e09613
Jan Koester <jan.koester@gmx.net>
2022-06-23 13:58:34 +0200

    basic functions pthread

--- src/string.cpp
+++ src/string.cpp
@@ -27,7 +27,28 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <stddef.h>
 
+
+#define SS (sizeof(size_t))
+#define ALIGN (sizeof(size_t)-1)
+#define ONES ((size_t)-1/UCHAR_MAX)
+#define HIGHS (ONES * (UCHAR_MAX/2+1))
+#define HASZERO(x) ((x)-ONES & ~(x) & HIGHS)
+
 extern "C" {
+
+    void *memchr(const void *src, int c, size_t n){
+        const unsigned char *s =(const unsigned char*) src;
+        c = (unsigned char)c;
+        for (; ((uintptr_t)s & ALIGN) && n && *s != c; s++, n--);
+        if (n && *s != c) {
+            const size_t *w;
+            size_t k = ONES * c;
+            for (w = (const void *)s; n>=SS && !HASZERO(*w^k); w++, n-=SS);
+            for (s = (const void *)w; n && *s != c; s++, n--);
+        }
+        return n ? (void *)s : 0;
+    }
+    
     void memcpy(void *dest, const void * src, size_t n){
         for (int i=0; i<n; i++)
             ((char *)dest)[i] = ((char*)src)[i];
